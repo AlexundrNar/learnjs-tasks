0a9415e198257ab6c2a1135fbcc327ea9009df30
NarAlex <sashabik@mail.ru>
2023-03-23 00:52:05 +0600

    9-13 chapters

---
+++ js/11-async/11-8/3.js
@@ -0,0 +1,12 @@
+// Есть «обычная» функция. Как можно внутри неё получить результат выполнения async–функции?
+async function wait() {
+  await new Promise(resolve => setTimeout(resolve, 1000));
+
+  return 10;
+}
+
+function f() {
+  wait().then(result => console.log(result));
+}
+
+f();
